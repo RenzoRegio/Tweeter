bf99193cd4ce7e3fba0247771a3904042fa5940b
RenzoRegio <renzorregio@gmail.com>
2021-03-12 19:58:30 +0800

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
+import React, { useState } from "react";
 import Router from "./Router";
+import { firebaseAuthorization } from "../firebase";
 
 function App() {
-  return <Router />;
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    firebaseAuthorization.currentUser
+  );
+  return <Router isLoggedIn={isLoggedIn} />;
 }
 
 export default App;
