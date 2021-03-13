73a020789aaede313ba1b5ecf00e13cd0ae132e3
RenzoRegio <renzorregio@gmail.com>
2021-03-13 10:01:24 +0800

    Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default () => {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={() => onSubmit()}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          onChange={onChange}
+          value={email}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          onChange={onChange}
+          value={password}
+        />
+        <input type="submit" value="Login" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
