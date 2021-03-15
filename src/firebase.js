c630f15dcf6c844fdd779b1dc2bb63ab7c6d5936
RenzoRegio <renzorregio@gmail.com>
2021-03-15 14:38:19 +0800

    Tweeting and DB setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,3 +15,4 @@ firebase.initializeApp(firebaseConfig);
 
 export const firebaseAuthorization = firebase.auth();
 export const firebaseProviders = firebase.auth;
+export const firebaseDB = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { firebaseDB } from "../firebase";
 
 const Home = () => {
-  return <span> Home </span>;
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setTweet(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await firebaseDB.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
