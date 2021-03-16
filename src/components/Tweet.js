480b8293b89c15fdf861b501114a0553fba7df9b
RenzoRegio <renzorregio@gmail.com>
2021-03-16 14:09:16 +0800

    Update tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { firebaseDB } from "../firebase";
 
 export default ({ tweetObj, userObj }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+
   const checkUser = (userId) => {
     const currentUserId = userObj.uid;
     if (currentUserId == userId) {
@@ -18,15 +21,48 @@ export default ({ tweetObj, userObj }) => {
     }
   };
 
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const updateTweet = (e) => {
+    e.preventDefault();
+    firebaseDB.collection("tweets").doc(tweetObj.id).update({ text: newTweet });
+    setEditing(false);
+  };
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewTweet(value);
+  };
+
   return (
     <div className={checkUser(tweetObj.userId) ? "my-tweet" : "tweet"}>
-      {checkUser(tweetObj.userId) && (
+      {checkUser(tweetObj.userId) && !editing ? (
         <div className="edit-buttons">
-          <i onClick={deleteTweet} class="fas fa-trash-alt"></i>
-          <i class="fas fa-edit"></i>
+          <i onClick={deleteTweet} className="fas fa-trash-alt"></i>
+          <i onClick={toggleEditing} className="fas fa-edit"></i>
         </div>
+      ) : null}
+      {editing ? (
+        <div className="update-form">
+          <form onSubmit={(e) => updateTweet(e)}>
+            <input
+              onChange={onChange}
+              type="text"
+              value={newTweet}
+              placeholder="Edit Tweet"
+              required
+            />
+            <button className="update-btn">
+              <i class="fab fa-twitter"></i>
+            </button>
+            <button onClick={toggleEditing}>Cancel</button>
+          </form>
+        </div>
+      ) : (
+        <h2>{tweetObj.text}</h2>
       )}
-      <h2>{tweetObj.text}</h2>
     </div>
   );
 };
