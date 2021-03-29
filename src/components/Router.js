999b7938c641992dbc757c4acc551e695ae73014
RenzoRegio <renzorregio@gmail.com>
2021-03-29 00:30:21 -0700

    Build

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 // Routes
 import Auth from "../routes/Auth";
